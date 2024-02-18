2ca0005c67864eef9c9fd5b88d8ad22a6b13fa69
Sahana Tankala <sahanatankala@gmail.com>
2024-02-18 00:24:47 -0500

    Better recoverable error handling

--- src/syntax/parser.rs
+++ src/syntax/parser.rs
@@ -11,60 +11,73 @@ use super::{
     Spanned,
 };
 
-type ParseResult<T> = anyhow::Result<(T, Vec<OwlParseError>)>;
 type InputIter = Peekable<std::vec::IntoIter<Spanned<Token>>>;
 
 /// Entry point for the Owl parser
 ///
 /// * `path`: The path to the source file
-pub fn owl_program_parser(path: &str) -> ParseResult<Program> {
+pub fn owl_program_parser(path: &str) -> anyhow::Result<Program> {
     let mut file = File::open(path)?;
     let mut source = String::new();
     file.read_to_string(&mut source)?;
 
     let mut lex = lexer(&source).into_iter().peekable();
 
-    parse_program(&mut lex)
+    let (prog, errs) = parse_program(&mut lex)?;
+    
+    // TODO: Produce error report with ariadne
+
+    Ok(prog)
 }
 
-fn parse_program(lex: &mut InputIter) -> Result<(Program, Vec<OwlParseError>), anyhow::Error> {
-    let (decls, errors) = parse_declarations(lex)?;
-    let mut errs = errors;
-    let (expr, mut errors) = parse_expression(lex)?;
-    errs.append(&mut errors);
+fn parse_program(lex: &mut InputIter) -> anyhow::Result<(Program, Vec<OwlParseError>)> {
+    let mut errors = vec![];
+    let decls = parse_declarations(lex, &mut errors)?;
+    let expr = parse_expression(lex, &mut errors)?;
 
-    Ok((Program::new(decls, expr), errs))
+    Ok((Program::new(decls, expr), errors))
 }
 
-fn parse_semicolon(lex: &mut InputIter) -> ParseResult<()> {
-    todo!()
+/// Attempts to parse the given token exactly.
+///
+/// If another token is found, we keep parsing as if we found the requested token.
+fn expect_tok(tok: Token, lex: &mut InputIter, errors: &mut Vec<OwlParseError>) -> anyhow::Result<()> {
+    match lex.next() {
+        Some((found, _)) if found == tok => Ok(()),
+        Some((other, sp)) => {
+            errors.push(OwlParseError::UnexpectedToken {
+                expected: tok,
+                found: (other, sp),
+            });
+
+            Ok(())
+        },
+        None => Err(OwlParseError::EndOfInput.into()),
+    }
 }
+
 /// Parses semicolon separated declarations, returning the declarations and any recoverable errors
 ///
 /// * `lex`: the token iterator
-fn parse_declarations(lex: &mut InputIter) -> ParseResult<Vec<Spanned<Declaration>>> {
+fn parse_declarations(lex: &mut InputIter, errors: &mut Vec<OwlParseError>) -> anyhow::Result<Vec<Spanned<Declaration>>> {
     let mut errors = vec![];
     let mut decs: Vec<Spanned<Declaration>> = vec![];
 
-    let last = loop {
+    loop {
         match lex.peek() {
-            Some((Token::Let, sp)) => {
+            Some((Token::Let, _)) => {
                 // parse a value Declaration
-                let (value_dec, mut errs) = parse_value_declaration(lex)?;
-                errors.append(&mut errs);
+                let value_dec = parse_value_declaration(lex, &mut errors)?;
                 decs.push(value_dec);
                 // parse a semicolon
-                let (_, mut errs) = parse_semicolon(lex)?;
-                errors.append(&mut errs);
+                let _ = expect_tok(Token::SemiColon, lex, &mut errors)?;
             }
-            Some((Token::Typ, sp)) => {
+            Some((Token::Typ, _)) => {
                 // parse a type Declaration
-                let (value_dec, mut errs) = parse_type_declaration(lex)?;
-                errors.append(&mut errs);
+                let value_dec = parse_type_declaration(lex, &mut errors)?;
                 decs.push(value_dec);
                 // parse a semicolon
-                let (_, mut errs) = parse_semicolon(lex)?;
-                errors.append(&mut errs);
+                let _ = expect_tok(Token::SemiColon, lex, &mut errors)?;
             }
             Some((Token::Error(_), _)) => {
                 let Some((Token::Error(bad), sp)) = lex.next() else {
@@ -72,26 +85,25 @@ fn parse_declarations(lex: &mut InputIter) -> ParseResult<Vec<Spanned<Declaratio
                 };
                 errors.push(OwlParseError::LexerError((bad, (sp.start..sp.end))))
             }
-            Some((other, sp)) => {
-                // no more declarations, we need to parse an expression
-                break other;
-            }
+            Some((_, _)) => break,
             // early EOI is unrecoverable
             None => return Err(OwlParseError::EndOfInput.into()),
         };
-    };
+    }
 
-    Ok((decs, errors))
+    Ok(decs)
 }
 
-fn parse_value_declaration(lex: &mut InputIter) -> ParseResult<Spanned<Declaration>> {
+fn parse_value_declaration(lex: &mut InputIter, errors: &mut Vec<OwlParseError>) -> anyhow::Result<Spanned<Declaration>> {
+    let _ = expect_tok(Token::Let, lex, errors)?;
+
     todo!()
 }
 
-fn parse_type_declaration(lex: &mut InputIter) -> ParseResult<Spanned<Declaration>> {
+fn parse_type_declaration(lex: &mut InputIter, errors: &mut Vec<OwlParseError>) -> anyhow::Result<Spanned<Declaration>> {
     todo!()
 }
 
-fn parse_expression(lex: &mut InputIter) -> ParseResult<Spanned<Expression>> {
+fn parse_expression(lex: &mut InputIter, errors: &mut Vec<OwlParseError>) -> anyhow::Result<Spanned<Expression>> {
     todo!()
 }
