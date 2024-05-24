0e8c84d2442645dd4dd48a2f86bb2485946e4ebf
Sahana Tankala <sahanatankala@gmail.com>
2024-05-24 18:08:18 -0400

    stopping for now???

--- src/codegen.rs
+++ src/codegen.rs
@@ -9,7 +9,10 @@ use crate::syntax::ast::Program;
 pub mod wasm;
 
 pub trait OwlBackend {
-    // TODO: This might return something, I'm not sure
+    /// Compiles a given [`Program`], writing the program to the output file.
+    ///
+    /// * `path`: The of the output file
+    /// * `program`: The [`Program`] to compile
     fn compile(path: &str, program: Program) -> anyhow::Result<()> {
         let mut outfile = File::create(path)?;
         let contents = Self::codegen(program)?;
@@ -17,5 +20,8 @@ pub trait OwlBackend {
         Ok(())
     }
 
+    /// Compiles a program into a byte vector.
+    ///
+    /// * `program`: The [`Program`] to compile
     fn codegen(program: Program) -> anyhow::Result<Vec<u8>>;
 }
--- src/codegen/wasm.rs
+++ src/codegen/wasm.rs
@@ -9,10 +9,26 @@ struct WasmBackend {
     ast: ast::Program,
 }
 
+type Bytes = Vec<u8>;
+
 // TODO: Magic Header
 
 impl WasmBackend {
     pub fn new(ast: ast::Program) -> Self {
         Self { ast }
+    }
+
+    pub fn emit_declaration(decl: ast::Declaration) -> Bytes {
+        todo!()
+    }
+}
+
+impl OwlBackend for WasmBackend {
+    fn codegen(program: ast::Program) -> anyhow::Result<Vec<u8>> {
+        // TODO:: compile the functions into a module
+
+        let function_bytes = program.0.iter().map(Self::emit_declaration(decl));
+
+        todo!()
     }
 }
